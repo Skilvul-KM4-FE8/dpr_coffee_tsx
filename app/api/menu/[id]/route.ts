91a6e6851e4e59cd4732619186f374d409b8a6d9
Muhammad Rafai <rafaimhd123@gmail.com>
2024-10-09 20:18:32 +0700

    add endpoint to update menu PATCH /api/menu/[id]

--- app/api/menu/[id]/route.ts
+++ app/api/menu/[id]/route.ts
@@ -2,7 +2,13 @@ import {prisma} from "@/lib/prisma"
 import { getAuth } from "@clerk/nextjs/server"
 import { NextRequest } from "next/server"
 
-export async function GET(req: Request, {params}: {params: {id: string}}) {
+type ParamsType = {
+    params: {
+        id: string
+    }
+}
+
+export async function GET(req: Request, {params}: ParamsType) {
     const id = params.id
     const auth = getAuth(req as NextRequest)
 
@@ -22,6 +28,57 @@ export async function GET(req: Request, {params}: {params: {id: string}}) {
             }
         })
 
+        if (!response) {
+            return new Response(JSON.stringify({message: "NOT FOUND",}), {
+                status: 404,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        }
+        
+        console.log(response)
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            } 
+        })
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({message: error,}), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+}
+
+export async function PATCH(req: Request, {params}: ParamsType) {
+    const id = params.id
+    const auth = getAuth(req as NextRequest)
+    const payload = await req.json()
+
+    if (!auth?.userId) {
+        return new Response(JSON.stringify({ message: "Unauthorized!"}), {
+            status: 401,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    try {
+        const response = prisma.menu.update({
+            where: {
+                id: id
+            },
+            data : {
+                name: payload.name,
+                price: payload.price
+            }
+        })
         if (!response) {
             return new Response(JSON.stringify({message: "NOT FOUND",}), {
                 status: 404,
