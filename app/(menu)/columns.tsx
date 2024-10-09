c860d02c238fbe154c81d5b489b6c1a33b84add0
Muhammad Rafai <rafaimhd123@gmail.com>
2024-10-09 10:49:34 +0700

    add skeleton for menu page

--- app/(menu)/columns.tsx
+++ app/(menu)/columns.tsx
@@ -2,10 +2,12 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { useEditMenu } from "@/features/menu/hooks/use-edit-menu";
 import { ArrowUpDown } from "lucide-react"
+
+import { useEditMenu } from "@/features/menu/hooks/use-edit-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
@@ -19,6 +21,25 @@ export type Menu = {
 };
 
 export const columns: ColumnDef<Menu>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox 
+        checked={
+          table.getIsAllPageRowsSelected() || (table.getIsSomeRowsSelected()) && "indeterminate"
+        }
+        onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+        aria-label="ini aria label"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox 
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row ini"
+      />
+    )
+  },
   {
     accessorKey: "name",
     header: ({ column }) => {
@@ -35,7 +56,7 @@ export const columns: ColumnDef<Menu>[] = [
   },
   {
     accessorKey: "price",
-    header: () => <div className="text-right">Price</div>,
+    header: () => <div className="text-left">Price</div>,
     cell: ({ row }) => {
       const amount = parseFloat(row.getValue("price"));
       const formatted = new Intl.NumberFormat("en-US", {
@@ -43,34 +64,35 @@ export const columns: ColumnDef<Menu>[] = [
         currency: "IDR",
       }).format(amount);
 
-      return <div className="text-right font-medium">{formatted}</div>;
+      return <div className="text-left font-medium">{formatted}</div>;
     },
   },
-  // {
-  //   id: "actions",
-  //   cell: ({ row }) => {
-  //     const payment = row.original;
+  {
+    id: "actions",
+    header: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
 
-  //     const { onOpen } = useEditMenu();
+      const { onOpen } = useEditMenu();
 
-  //     return (
-  //       <>
-  //         <DropdownMenu>
-  //           <DropdownMenuTrigger asChild>
-  //             <Button variant="ghost" className="h-8 w-8 p-0">
-  //               <span className="sr-only">Open menu</span>
-  //               <MoreHorizontal className="h-4 w-4" />
-  //             </Button>
-  //           </DropdownMenuTrigger>
-  //           <DropdownMenuContent align="end">
-  //             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-  //             <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id)}>Copy payment ID</DropdownMenuItem>
-  //             <DropdownMenuSeparator />
-  //             <DropdownMenuItem onClick={onOpen}>Edit</DropdownMenuItem>
-  //           </DropdownMenuContent>
-  //         </DropdownMenu>
-  //       </>
-  //     );
-  //   },
-  // },
+      return (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id)}>Copy payment ID</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={onOpen}>Edit</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
+      );
+    },
+  },
 ];
--- app/(menu)/page.tsx
+++ app/(menu)/page.tsx
@@ -4,8 +4,9 @@ import { DataTable } from "@/app/(menu)/data-table";
 import { Button } from "@/components/ui/button";
 import { useNewMenu } from "@/features/menu/hooks/use-new-menu";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { useGetMenus } from "@/features/menu/api/use-get-menus";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function DemoPage() {
   let dataMenu = [
@@ -25,6 +26,36 @@ export default function DemoPage() {
   const { isOpen, onOpen, onClose } = useNewMenu();
   console.log("Data dari Api:",menuData)
   console.log("Data local:", dataMenu)
+
+  if (menuQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl w-full pb-10 -mt-24 flex justify-center items-center mx-auto">
+            {/* <div>
+              <Loader2 className="size-10 lg:size-14 text-muted-foreground animate-spin inline-block" />
+            </div> */}
+        <Card className="w-full border-none drop-shadow-sm">
+            <CardHeader className="flex gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+              <CardTitle className="text-xl line-clamp-1">
+                  <Skeleton className="h-10 w-28 lg:w-48" />
+              </CardTitle>
+              <Skeleton className="h-10 w-full lg:w-36" />
+            {/* <Skeleton className="h-10 w-28 md:w-48" /> */}
+          </CardHeader>
+          <CardContent className="grid gap-y-2">
+            <div className="flex justify-between">
+              <Skeleton className="h-10 w-2/5" />
+              <Skeleton className="h-10 w-full lg:w-28" />
+            </div>
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-8 w-full" />
+            <Skeleton className="h-8 w-full" />
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+
   return (
     <div className="mx-auto max-w-screen-2xl w-full pb-10 -mt-24">
                 <Card className="border-none drop-shadow-sm ">
