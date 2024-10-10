17427ee2c74bf6436350ec7567d35d679a79ec3c
Muhammad Rafai <rafaimhd123@gmail.com>
2024-10-10 11:40:37 +0700

    add endpoint DELETE /api/menu/[id]

--- app/api/menu/[id]/route.ts
+++ app/api/menu/[id]/route.ts
@@ -107,3 +107,40 @@ export async function PATCH(req: Request, {params}: ParamsType) {
         })
     }
 }
+
+export async function DELETE(req: Request, {params}: ParamsType) {
+    const id = params.id
+    const auth = getAuth(req as NextRequest)
+    
+    if (!auth?.userId) {
+        return new Response(JSON.stringify({message: "Unauthorized"}), {
+            status: 401,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+    }
+
+    try {
+        const response = await prisma.menu.delete({
+            where: {
+                id: id
+            }
+        })
+        console.log(response)
+        return new Response(JSON.stringify({message: "Success", response}), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({message: error,}), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+}
