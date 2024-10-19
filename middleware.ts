ce2a1e7d7dc27b048fc138b5e156602e40cabb84
santosoo <sanskun02pg@gmail.com>
2024-10-19 21:50:09 +0700

    fixing category in add new menu and fixing middleware /(All) callback link

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-
-const isProtectedRoute = createRouteMatcher(
-    [
-        '/',
-        // '/api(.*)'
-    ]
-)
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/api/menu",
+  "/transaction",
+  "api/transaction/",
+  "/management",
+  "/overview",
+  // '/api(.*)'
+]);
 
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect()
-})
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
